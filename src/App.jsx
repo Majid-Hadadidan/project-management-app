780902ec262792266a7259baaade69ca4cfe0bce
majidH <majid.hadadian@gmail.com>
2024-03-24 23:21:32 +0330

    add sideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-
+import ProjectsSidebar from "./components/ProjectsSidebar";
 export default function App() {
   return (
-<>
-
-</>
+    <main className="my-8">
+      <ProjectsSidebar />
+    </main>
   );
 }
